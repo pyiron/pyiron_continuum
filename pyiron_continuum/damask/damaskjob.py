b8a546522c366c6bb5b2083b8e219af4e81d1f6a
Ahmed-Aslam <37520754+Ahmed-Aslam@users.noreply.github.com>
2021-06-30 08:01:40 +0200

    Add wrapper
    To do in next pull request! 
    # Use DataContainer instead of InputList
    # Replace to_hdf, from_hdf
    # Refactoring the code

--- pyiron_continuum/damask/damaskjob.py
+++ pyiron_continuum/damask/damaskjob.py
@@ -1,4 +1,9 @@
-from pyiron_base import Project, GenericJob, GenericParameters
+# To do in next pull request! 
+# Refactor the code according to Liams suggestion. 
+# Use DataContainer instead of InputList
+# Replace to_hdf, from_hdf
+
+from pyiron_base import Project, GenericJob, GenericParameters, InputList
 import numpy as np
 import matplotlib.pyplot as plt
 from damask import Config
@@ -13,53 +18,98 @@ import os
 class DAMASKjob(GenericJob):
     def __init__(self, project, job_name):
         super(DAMASKjob, self).__init__(project, job_name)
-        self.input = GenericParameters(table_name="input")
-        self.input['C11'] = 0
-        self.input['C12'] = 0
-        self.input['C44'] = 0
-        self.input['grid'] = np.array([16,16,16])
-        self.input['size'] = np.array([1.0,1.0,1.0])
-        self.input['grains'] = 20
-        self.executable = "DAMASK_grid -l tensionX.load -g damask.vtr"
+        self.input = InputList()
         self._material = None 
-        self._tension = None 
+        self._load = None
+        self._geometry = None
+        self._damask_results = None
+        self.input.create_group('geometry')
+        self.input.create_group('material')
+        self.input.create_group('load')
+        self.executable = "DAMASK_grid -l tensionX.yaml -g damask.vtr"
         
     @property
     def material(self):
         return self._material
     
     @material.setter
-    def material(self, mat):
-        self._material = mat
+    def material(self, path=None):
+        with open(path) as f:
+            self._material = yaml.load(f, Loader=yaml.FullLoader)
+        self.material_inputlist()
         
     @property
-    def tension(self):
-        return self._tension
+    def load(self):
+        return self._load
     
-    @tension.setter
-    def tension(self, tension):
-        self._tension = tension
+    @load.setter
+    def load(self, path=None):
+        with open(path) as f:
+            self._load = yaml.load(f, Loader=yaml.FullLoader)
+        self.load_inputlist()
+    
+    @property
+    def path(self):
+        return self._path
+    
+    @path.setter
+    def path(self, path):
+        self._path = path
+        self.check_path()
+       
+    def check_path(self):
+        if self._path is not None:
+            try:
+                with open(os.path.join(self._path, 'damask.vtr')) as f:
+                    geometry = f.readlines()
+                self.geometry = geometry
+                with open(os.path.join(self._path, 'material.yaml')) as f:
+                    material = yaml.load(f, Loader=yaml.FullLoader)
+                self.material = material
+                with open(os.path.join(self._path, 'tensionX.yaml')) as f:
+                    load = f.readlines()
+                self.load = load
+            except:
+                pass
+        else:
+            pass
+                
+    def load_inputlist(self):
+        if isinstance(self._load, type(None)):
+            raise ValueError('job.load file not specified')
+        else:
+            self.input.load = InputList(self._load)
+            
+    def material_inputlist(self):
+        if isinstance(self._material, type(None)):
+            raise ValueError('job.material file not specified')
+        else:
+            self.input.material = InputList(self._material)
+    
+    def load_write(self):
+        load = self.input.load.to_builtin()
+        with open(os.path.join(self.working_directory, 'tensionX.yaml'), "w") as f:
+                    yaml.dump(load , f)
         
-    def write_input(self): 
+    def material_write(self):
+        material = self.input.material.to_builtin()
         with open(os.path.join(self.working_directory, 'material.yaml'), "w") as f:
-            yaml.dump(self._material, f)
-        with open(os.path.join(self.working_directory, 'tensionX.load'), "w") as f:
-            f.writelines(self._tension)
-        seed = seeds.from_random(self.input['size'], self.input['grains'])
-        new_geom = Grid.from_Voronoi_tessellation(self.input['grid'], self.input['size'], seed)
-        # new_geom.save_ASCII(os.path.join(self.working_directory, "damask.geom"))
+                    yaml.dump(material , f)
+    
+    def geometry_write(self):
+        seed = seeds.from_random(self.input.geometry['size'], self.input.geometry['grains'])
+        new_geom = Grid.from_Voronoi_tessellation(self.input.geometry['grid'], self.input.geometry['size'], seed)
+        #new_geom.save_ASCII(os.path.join(self.working_directory, "damask.vtr"))
         new_geom.save(os.path.join(self.working_directory, "damask"))
-        C_matrix = [self.input['C11']*1e9, self.input['C12']*1e9, self.input['C44']*1e9]
-        elasticity={}
-        elasticity.update({'type': 'hooke'})
-        elastic_constants = {'C_11': C_matrix[0], 'C_12': C_matrix[1], 'C_44': C_matrix[2]}
-        elasticity.update(elastic_constants)
-        mat = Config.load(os.path.join(self.working_directory, 'material.yaml'))
-        mat['phase']['Aluminum']['elasticity'] = elasticity
-        mat.save(os.path.join(self.working_directory, 'material.yaml'))
+    
+    def write_input(self):
         
+        self.load_write()
+        self.geometry_write()
+        self.material_write()
+             
     def collect_output(self): 
-        pass 
+        pass
     
     def to_hdf(self, hdf=None, group_name=None): 
         super().to_hdf(
@@ -69,7 +119,8 @@ class DAMASKjob(GenericJob):
         with self.project_hdf5.open("input") as h5in:
             self.input.to_hdf(h5in)
             h5in["material"] = self._material
-            h5in["tension"] = self._tension
+            h5in["load"] = self._load
+        self.status.finished = True
 
     def from_hdf(self, hdf=None, group_name=None): 
         super().from_hdf(
@@ -79,21 +130,28 @@ class DAMASKjob(GenericJob):
         with self.project_hdf5.open("input") as h5in:
             self.input.from_hdf(h5in)
             self._material = h5in["material"]
-            self._tension = h5in["tension"]
+            self._load = h5in["load"]
     
-    @property
-    def output_file(self):
-        file_name = os.path.join(self.working_directory, "damask_tensionX.hdf5")
-        if self.status.finished and os.path.exists(file_name):
-            return file_name
+    def load_results(self, file_name):
+        if self._damask_results is None:
+            self._damask_results = self.output
+            self._damask_results.add_stress_Cauchy()
+            self._damask_results.add_strain()
+            self._damask_results.add_equivalent_Mises('sigma')
+            self._damask_results.add_equivalent_Mises('epsilon_V^0.0(F)')
+            self._damask_results.add_calculation('avg_sigma',"np.average(#sigma_vM#)")
+            self._damask_results.add_calculation('avg_epsilon',"np.average(#epsilon_V^0.0(F)_vM#)")
+            self._damask_results.save_VTK(['sigma','epsilon_V^0.0(F)','sigma_vM','epsilon_V^0.0(F)_vM'])
+        return self._damask_results
+    
     
     @property
     def output(self):
-        file_name = self.output_file
-        if file_name is not None:
-            return Result(file_name)
-
-    def eval_stress(self):
+        self._file_name = os.path.join(self.working_directory, "damask_tensionX.hdf5")
+        #if file_name is not None:
+        return Result(self._file_name)
+    
+    def stress(self):
         """
         return the stress as a numpy array
         Parameters
@@ -101,18 +159,18 @@ class DAMASKjob(GenericJob):
         job_file : str
           Name of the job_file
         """
-        file_name = self.output_file
-        if file_name is not None:
-            d = Result(file_name)
-            stress_path = d.get_dataset_location('avg_sigma')
+        output = self.output
+        self.load_results(self._file_name)
+        if output is not None:
+            stress_path = output.get_dataset_location('avg_sigma')
             stress = np.zeros(len(stress_path))
-            hdf = h5py.File(d.fname)
+            hdf = h5py.File(output.fname)
             for count,path in enumerate(stress_path):
                 stress[count] = np.array(hdf[path])
             stress = np.array(stress)/1E6
             return stress
 
-    def eval_strain(self):
+    def strain(self):
         """
         return the strain as a numpy array
         Parameters
@@ -120,38 +178,46 @@ class DAMASKjob(GenericJob):
         job_file : str
           Name of the job_file
         """
-        file_name = self.output_file
-        if file_name is not None:
-            d = Result(file_name)
-            stress_path = d.get_dataset_location('avg_sigma')
+        output = self.output
+        self.load_results(self._file_name)
+        if output is not None:
+            stress_path = output.get_dataset_location('avg_sigma')
             strain = np.zeros(len(stress_path))
-            hdf = h5py.File(d.fname)
+            hdf = h5py.File(output.fname)
             for count,path in enumerate(stress_path):
-                strain[count] = np.array(hdf[path.split('avg_sigma')[0]     + 'avg_epsilon'])
-
+                strain[count] = np.array(hdf[path.split('avg_sigma')[0]+ 'avg_epsilon'])
             return strain  
         
-    def plot(self):
+    def plot_stress_strain(self):
         """
         Plot the stress strain curve from the job file
 
         Parameters
         ----------
-        job_file : str
-        Name of the job_file
+        self.stress
+        self.strain
         """
-        file_name = self.output_file
-        if file_name is not None:
-            d = Result(file_name)
-            stress_path = d.get_dataset_location('avg_sigma')
-            stress = np.zeros(len(stress_path))
-            strain = np.zeros(len(stress_path))
-            hdf = h5py.File(d.fname)
-            for count, path in enumerate(stress_path):
-                stress[count] = np.array(hdf[path])
-                strain[count] = np.array(hdf[path.split('avg_sigma')[0]     + 'avg_epsilon'])
+        stress = self.stress()
+        strain = self.strain()
+        plt.plot(strain,stress,linestyle='-',linewidth='2.5')
+        plt.xlabel(r'$\varepsilon_{VM} $',fontsize=18)
+        plt.ylabel(r'$\sigma_{VM}$ (MPa)',fontsize=18)
+     
+    def mesh(self, inc=80):
+        """
+        Plot the stress strain curve from the job file
 
-            stress = np.array(stress)/1E6
-            plt.plot(strain,stress,linestyle='-',linewidth='2.5')
-            plt.xlabel(r'$\varepsilon_{VM} $',fontsize=18)
-            plt.ylabel(r'$\sigma_{VM}$ (MPa)',fontsize=18)
+        Parameters
+        ----------
+        inc =  results per time increment
+        self.stress
+        self.strain
+        """
+        mesh = pv.read(os.path.basename(self.output_file.split('.')[0]) + f'_inc{inc}.vtr')
+        #from itkwidgets import view
+        #import itk
+        #mesh
+        #pl = pv.PlotterITK()
+        #pl.add_mesh(mesh)
+        #pl.show()
+        return mesh
