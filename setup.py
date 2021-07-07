bb987d7aed06d4b7ce9e0a44eeac97f99f64cc15
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-07-07 09:33:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=[
         'matplotlib==3.4.2',
         'numpy==1.21.0',
-        'pyiron_base==0.2.18'
+        'pyiron_base==0.2.19'
     ],
     extras_require={'fenics': [
         'fenics==2019.1.0',
