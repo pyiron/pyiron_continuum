70837e5d306789ba71a08b0ae6a90d942e603901
Sam Waseda <o.waseda@mpie.de>
2021-08-24 15:15:03 +0200

    add test

--- tests_integration/elasticity/test_elasticity.py
+++ tests_integration/elasticity/test_elasticity.py
@@ -1,6 +1,14 @@
 import numpy as np
 import unittest
 from pyiron_continuum.elasticity.linear_elasticity import LinearElasticity
+from pyiron_continuum.elasticity.tools import *
+
+def create_random_C():
+    C = np.zeros((6,6))
+    C[:3,:3] = np.random.random()
+    C[:3,:3] += np.random.random()*np.eye(3)
+    C[3:,3:] = np.random.random()*np.eye(3)
+    return C_from_voigt(C)
 
 class TestFenicsTutorials(unittest.TestCase):
     def test_frame(self):
@@ -8,6 +16,19 @@ class TestFenicsTutorials(unittest.TestCase):
         self.assertAlmostEqual(np.linalg.det(medium.frame), 1)
         medium.frame = np.random.random((3,3))
         self.assertAlmostEqual(np.linalg.det(medium.frame), 1)
+
+    def test_rotation(self):
+        elastic_tensor = create_random_C()
+        epsilon = np.random.random((3,3))
+        epsilon += epsilon.T
+        sigma = np.einsum('ijkl,kl->ij', elastic_tensor, epsilon)
+        medium = LinearElasticity(elastic_tensor)
+        medium.frame = np.array([[1,1,1],[1,0,-1]])
+        sigma = np.einsum('iI,jJ,IJ->ij', medium.frame, medium.frame, sigma)
+        sigma_calc = np.einsum(
+            'ijkl,kK,lL,KL->ij', medium.elastic_tensor, medium.frame, medium.frame, epsilon
+        )
+        self.assertTrue(np.isclose(sigma-sigma_calc, 0).all())
 
     def test_not_initialized(self):
         medium = LinearElasticity()
