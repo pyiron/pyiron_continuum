61ebc11394020dc2d1a758831a34b1ef7bbdb2c9
Sam Waseda <o.waseda@mpie.de>
2021-08-26 20:02:20 +0200

    reduce project.py

--- pyiron_continuum/project.py
+++ pyiron_continuum/project.py
@@ -28,62 +28,6 @@ s = Settings()
 
 
 class Project(ProjectCore):
-    """
-    The project is the central class in pyiron, all other objects can be created from the project object.
-
-    Args:
-        path (GenericPath, str): path of the project defined by GenericPath, absolute or relative (with respect to
-                                     current working directory) path
-        user (str): current pyiron user
-        sql_query (str): SQL query to only select a subset of the existing jobs within the current project
-        default_working_directory (bool): Access default working directory, for ScriptJobs this equals the project
-                                     directory of the ScriptJob for regular projects it falls back to the current
-                                     directory.
-
-    Attributes:
-
-        .. attribute:: root_path
-
-            the pyiron user directory, defined in the .pyiron configuration
-
-        .. attribute:: project_path
-
-            the relative path of the current project / folder starting from the root path
-            of the pyiron user directory
-
-        .. attribute:: path
-
-            the absolute path of the current project / folder
-
-        .. attribute:: base_name
-
-            the name of the current project / folder
-
-        .. attribute:: history
-
-            previously opened projects / folders
-
-        .. attribute:: parent_group
-
-            parent project - one level above the current project
-
-        .. attribute:: user
-
-            current unix/linux/windows user who is running pyiron
-
-        .. attribute:: sql_query
-
-            an SQL query to limit the jobs within the project to a subset which matches the SQL query.
-
-        .. attribute:: db
-
-            connection to the SQL database
-
-        .. attribute:: job_type
-
-            Job Type object with all the available job types: ['XXX']
-    """
-
     def __init__(self, path="", user=None, sql_query=None, default_working_directory=False):
         super(Project, self).__init__(
             path=path,
@@ -95,29 +39,6 @@ class Project(ProjectCore):
         self._creator = Creator(self)
         # TODO: instead of re-initialzing, auto-update pyiron_base creator with factories, like we update job class
         #  creation
-
-    def create_job(self, job_type, job_name, delete_existing_job=False):
-        """
-        Create one of the following jobs:
-        - ‘XXX': job type XXX
-
-        Args:
-            job_type (str): job type can be ['XXX’]
-            job_name (str): name of the job
-
-        Returns:
-            GenericJob: job object depending on the job_type selected
-        """
-        job = JobType(
-            job_type,
-            project=ProjectHDFio(project=self.copy(), file_name=job_name),
-            job_name=job_name,
-            job_class_dict=self.job_type.job_class_dict,
-            delete_existing_job=delete_existing_job,
-        )
-        if self.user is not None:
-            job.user = self.user
-        return job
 
     def load_from_jobpath(self, job_id=None, db_entry=None, convert_to_object=True):
         """
@@ -166,6 +87,8 @@ class Project(ProjectCore):
 
         """
         ProjectGUI(self)
+
+Project.__doc__ = ProjectCore.__doc__
 
 
 class MediumFactory(PyironFactory):
