4fc6ad7000fbda86b547614e4508b7b6a73fe233
Sam Waseda <o.waseda@mpie.de>
2021-08-26 19:59:18 +0200

    add comments to tests

--- tests_integration/elasticity/test_eschelby.py
+++ tests_integration/elasticity/test_eschelby.py
@@ -16,18 +16,23 @@ def create_random_HL(b=None):
 class TestEschelby(unittest.TestCase):
     def test_p(self):
         hl = create_random_HL()
-        self.assertTrue(np.isclose(np.absolute(np.linalg.det(hl._get_pmat(hl.p))), 0).all())
+        self.assertTrue(
+            np.allclose(np.absolute(np.linalg.det(hl._get_pmat(hl.p))), 0),
+            'p-matrix has a full dimension'
+        )
 
     def test_Ak(self):
         hl = create_random_HL()
         self.assertTrue(
-            np.isclose(np.absolute(np.einsum('nk,nik->ni', hl.Ak, hl._get_pmat(hl.p))), 0).all()
+            np.allclose(np.absolute(np.einsum('nk,nik->ni', hl.Ak, hl._get_pmat(hl.p))), 0),
+            'Ak is not the kernel of the p-matrix'
         )
 
     def test_DAk(self):
         hl = create_random_HL()
         self.assertTrue(
-            np.isclose(np.real(np.einsum('nk,n->k', hl.Ak, hl.D)), hl.burgers_vector).all()
+            np.isclose(np.real(np.einsum('nk,n->k', hl.Ak, hl.D)), hl.burgers_vector).all(),
+            'Magnitude not corresponding to the Burgers vector'
         )
 
     def test_Dq(self):
@@ -36,7 +41,8 @@ class TestEschelby(unittest.TestCase):
         F += hl.elastic_tensor[:,1,:,0]
         F = np.einsum('nik,nk->ni', F, hl.Ak)
         self.assertTrue(
-            np.isclose(np.real(np.einsum('nk,n->k', F, hl.D)), 0).all()
+            np.allclose(np.real(np.einsum('nk,n->k', F, hl.D)), 0),
+            'Equilibrium condition not satisfied'
         )
 
     def test_displacement(self):
@@ -44,7 +50,8 @@ class TestEschelby(unittest.TestCase):
         positions = (np.random.random((100, 2))-0.5)*10
         d_analytical = np.arctan2(*positions.T[:2][::-1])/2/np.pi
         self.assertTrue(
-            np.all(np.absolute(hl.get_displacement(positions)[:,-1]-d_analytical) < 1.0e-4)
+            np.all(np.absolute(hl.get_displacement(positions)[:,-1]-d_analytical) < 1.0e-4),
+            'Screw dislocation displacement field not reproduced'
         )
 
     def test_strain(self):
@@ -52,11 +59,13 @@ class TestEschelby(unittest.TestCase):
         positions = (np.random.random((100, 2))-0.5)*10
         strain_analytical = positions[:,0]/np.sum(positions**2, axis=-1)/4/np.pi
         self.assertTrue(
-            np.all(np.absolute(hl.get_strain(positions)[:,1,2]-strain_analytical) < 1.0e-4)
+            np.all(np.absolute(hl.get_strain(positions)[:,1,2]-strain_analytical) < 1.0e-4),
+            'Screw dislocation strain field (yz-component) not reproduced'
         )
         strain_analytical = -positions[:,1]/np.sum(positions**2, axis=-1)/4/np.pi
         self.assertTrue(
-            np.all(np.absolute(hl.get_strain(positions)[:,0,2]-strain_analytical) < 1.0e-4)
+            np.all(np.absolute(hl.get_strain(positions)[:,0,2]-strain_analytical) < 1.0e-4),
+            'Screw dislocation strain field (xz-component) not reproduced'
         )
 
 if __name__ == "__main__":
