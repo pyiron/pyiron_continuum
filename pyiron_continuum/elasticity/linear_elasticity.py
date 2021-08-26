418f2527cfd8c0fecf7a58c3d9e56c4c21e2373e
Sam Waseda <o.waseda@mpie.de>
2021-08-26 19:25:08 +0200

    remove constants initialization

--- pyiron_continuum/elasticity/linear_elasticity.py
+++ pyiron_continuum/elasticity/linear_elasticity.py
@@ -28,21 +28,6 @@ def value_or_none(func):
             return func(self)
     return f
 
-def is_initialized(func):
-    def f(self):
-        if self._elastic_tensor is None:
-            v = np.sum([param is not None for param in [
-                self._lame_coefficient,
-                self._shear_modulus,
-                self._bulk_modulus,
-                self._poissons_ratio,
-                self._youngs_modulus
-            ]])
-            if v < 2:
-                return None
-        return func(self)
-    return f
-
 class LinearElasticity:
     """
     Linear elastic field class based on the 3x3x3x3 elastic tensor C_ijkl:
@@ -73,27 +58,17 @@ class LinearElasticity:
     >>> print(medium.get_dislocation_stress(random_positions, burgers_vector))
 
     """
-    def __init__(self, elastic_tensor=None):
+    def __init__(self, elastic_tensor):
         """
         Args:
 
             elastic_tensor ((3,3,3,3)- or (6,6)-array): Elastic tensor (in C_ijkl notation or
                 Voigt notation).
 
-        It is not mandatory to set the `elastic_tensor` during the initialization. Instead, at
-        least two of the elastic constants (such as `youngs_modulus`, `poissons_ratio` or
-        `shear_modulus`) can be set and `elastic_tensor` will be calculated automatically. If
-        only two constants are given, the system will be isotropic. If more than two constants
-        are given, it will be anisotropic (if the constants are not correlated).
         """
         self.elastic_tensor = elastic_tensor
         self._isotropy_tolerance = 1.0e-4
         self._rotation = np.eye(3)
-        self._lame_coefficient = None
-        self._shear_modulus = None
-        self._bulk_modulus = None
-        self._poissons_ratio = None
-        self._youngs_modulus = None
         self._eschelby = None
 
     @property
@@ -175,7 +150,6 @@ class LinearElasticity:
         return np.linalg.inv(self.elastic_tensor_voigt)
 
     @property
-    @is_initialized
     def zener_ratio(self):
         """
         Zener ratio or the anistropy index. If 1, the medium is isotropic. If isotropic, the
@@ -199,137 +173,41 @@ class LinearElasticity:
         self._isotropy_tolerance = value
 
     @property
-    @is_initialized
     def _is_isotropic(self):
         return np.absolute(self.zener_ratio-1) < self.isotropy_tolerance
 
     @property
-    @is_initialized
-    def lame_coefficient(self):
-        """
-        Lame's first parameter. It is calculated either from Young's modulus and Poisson's ratio
-        or Young's modulus and bulk modulus (depending on what is available)
-        """
-        if self._lame_coefficient is None:
-            if self.youngs_modulus is not None:
-                if self.poissons_ratio is not None:
-                    self._lame_coefficient = self.youngs_modulus*self.poissons_ratio
-                    self._lame_coefficient /= 1+self.poissons_ratio
-                    self._lame_coefficient /= 1-2*self.poissons_ratio
-                elif self.bulk_modulus is not None:
-                    self._lame_coefficient = 3*self.bulk_modulus
-                    self._lame_coefficient *= 3*self.bulk_modulus-self.youngs_modulus
-                    self._lame_coefficient /= 9*self.bulk_modulus-self.youngs_modulus
-        return self._lame_coefficient
-
-    @lame_coefficient.setter
-    def lame_coefficient(self, value):
-        if value < 0:
-            raise ValueError('lame_coefficient must be a positive float')
-        self._lame_coefficient = value
-
-    @property
-    @is_initialized
     def shear_modulus(self):
         """
-        Shear modulus (also known as Lame's second parameter). It is calculated from the
-        average shear components of the compliance matrix if the elastic tensor is available.
-        Otherwise it is calculated either from Lame's first parameter and Young's modulus and
-        Poisson's ratio or Lame's first parameter and Poisson's ratio (depending on what is
-        available)
+        Returns:
+            ((3,)-array): yz-, xz-, xy-components of shear modulus
         """
-        if self._shear_modulus is None:
-            if self._elastic_tensor is not None:
-                self._shear_modulus = 1/self.compliance_matrix[3:,3:].diagonal().mean()
-            elif self.lame_coefficient is not None:
-                if self.youngs_modulus is not None:
-                    R = self.youngs_modulus**2
-                    R += 9*self.lame_coefficient**2
-                    R += 2*self.youngs_modulus*self.lame_coefficient
-                    R = np.sqrt(R)
-                    self._shear_modulus = (self.youngs_modulus-3*self.lame_coefficient+R)/4
-                elif self.poissons_ratio is not None:
-                    self._shear_modulus = self.lame_coefficient*(1-2*self.poissons_ratio)
-                    self._shear_modulus /= 2*self.poissons_ratio
-        return self._shear_modulus
-
-    @shear_modulus.setter
-    def shear_modulus(self, value):
-        if value < 0:
-            raise ValueError('shear_modulus must be a positive float')
-        self._shear_modulus = value
+        return 1/self.compliance_matrix[3:,3:].diagonal()
 
     @property
-    @is_initialized
     def bulk_modulus(self):
         """
-        Bulk modulus. It is calculated either from shear modulus and Lame's first parameter or
-        shear modulus and Young's modulus (depending on what is available)
+        Returns:
+            (float): Bulk modulus
         """
-        if self._bulk_modulus is None:
-            if self.shear_modulus is not None:
-                if self.lame_coefficient is not None:
-                    self._bulk_modulus = self.lame_coefficient+2*self.shear_modulus/3
-                elif self.youngs_modulus is not None:
-                    self._bulk_modulus = self.youngs_modulus*self.shear_modulus
-                    self._bulk_modulus /= 3*(3*self.youngs_modulus-self.shear_modulus)
-        return self._bulk_modulus
-
-    @bulk_modulus.setter
-    def bulk_modulus(self, value):
-        if value < 0:
-            raise ValueError('bulk_modulus must be a positive float')
-        self._bulk_modulus = value
+        return 3*(1-2*self.poissons_ratio.mean())/self.youngs_modulus.mean()
 
     @property
-    @is_initialized
     def poissons_ratio(self):
         """
-        Poisson's ratio. If the elastic tensor is available, it is calculated from the compliance
-        matrix. Otherwise it is calculated either from bulk modulus and shear modulus or from bulk
-        modulus and lame coefficient (depending on what is available)
+        Returns:
+            ((3,)-array): yz-, xz-, xy-components of Poisson's ratio
         """
-        if self._poissons_ratio is None:
-            if self._elastic_tensor is not None:
-                self._poissons_ratio = -(self.compliance_matrix[:3,:3].sum()*self.youngs_modulus-3)/6
-            elif self.bulk_modulus is not None:
-                if self.shear_modulus is not None:
-                    self._poissons_ratio = (3*self.bulk_modulus-2*self.shear_modulus)
-                    self._poissons_ratio /= 2*(3*self.bulk_modulus+self.shear_modulus)
-                elif self.lame_coefficient is not None:
-                    self._poissons_ratio = self.lame_coefficient
-                    self._poissons_ratio /= 3*self.bulk_modulus-self.lame_coefficient
-        return self._poissons_ratio
-
-    @poissons_ratio.setter
-    def poissons_ratio(self, value):
-        if value < 0:
-            raise ValueError('poissons_ratio must be a positive float')
-        self._poissons_ratio = value
+        nu = -self.compliance_matrix[:3,:3]*self.youngs_modulus
+        return np.array([nu[1,2], nu[0,2], nu[0,1]])
 
     @property
-    @is_initialized
     def youngs_modulus(self):
         """
-        Young's modulus. If the elastic tensor is available, it is calculated from the compliance
-        matrix. Otherwise it is calculated either from Poisson's ratio and bulk modulus or from
-        Poisson's ratio and shear modulus (depending on what is available)
+        Returns:
+            ((3,)-array): xx-, yy-, zz-components of Young's modulus
         """
-        if self._youngs_modulus is None:
-            if self._elastic_tensor is not None:
-                self._youngs_modulus = 1/self.compliance_matrix[:3,:3].diagonal().mean()
-            elif self.poissons_ratio is not None:
-                if self.bulk_modulus is not None:
-                    self._youngs_modulus = 3*self.bulk_modulus*(1-2*self.poissons_ratio)
-                elif self.shear_modulus is not None:
-                    self._youngs_modulus = 2*self.shear_modulus*(1+self.poissons_ratio)
-        return self._youngs_modulus
-
-    @youngs_modulus.setter
-    def youngs_modulus(self, value):
-        if value < 0:
-            raise ValueError('youngs_modulus must be a positive float')
-        self._youngs_modulus = value
+        return 1/self.compliance_matrix[:3,:3].diagonal()
 
     def get_greens_function(
         self, positions, derivative=0, fourier=False, n_mesh=100, isotropic=False, optimize=True
@@ -353,7 +231,7 @@ class LinearElasticity:
             ((n,3)-array): Green's function values for the given positions
         """
         if isotropic or self._is_isotropic:
-            C = Isotropic(self.poissons_ratio, self.shear_modulus, optimize=optimize)
+            C = Isotropic(self.poissons_ratio.mean(), self.shear_modulus.mean(), optimize=optimize)
         else:
             C = Anisotropic(self.elastic_tensor, n_mesh=n_mesh, optimize=optimize)
         return C.get_greens_function(positions, derivative, fourier)
