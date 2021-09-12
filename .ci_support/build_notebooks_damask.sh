fc27bff05fe122cf94050d2b2c2ca20a4d2d039a
muh-hassani <mhassani86@gmail.com>
2021-09-12 13:03:12 +0200

    adding bash and environment for damask tests

---
+++ .ci_support/build_notebooks_damask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for f in $(cat .ci_support/exclude); do 
+    rm "notebooks/$f";     
+done;
+# execute notebooks
+i=0;
+cd notebooks
+for notebook in $(ls ./damask_*.ipynb); do
+    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
+done;
+
+# push error to next level
+if [ $i -gt 0 ]; then
+    exit 1;
+fi;
