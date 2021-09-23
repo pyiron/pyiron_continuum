c66be7be2da7485345661ab2a777b67d96b79e45
Liam Huber <liam.huber@gmail.com>
2021-09-23 11:52:19 +0200

    Exploit toolkits and update demo

--- pyiron_continuum/__init__.py
+++ pyiron_continuum/__init__.py
@@ -7,6 +7,10 @@ from pyiron_continuum.project import Project
 from pyiron_continuum.schroedinger.mesh import RectMesh
 from pyiron_continuum.schroedinger.potentials import Potential
 
+from pyiron_base import Project as ProjectBase
+from pyiron_continuum.toolkit import ContinuumTools
+ProjectBase.register_tools('continuum', ContinuumTools)
+
 # Make classes available for new pyiron version
 JOB_CLASS_DICT['Fenics'] = 'pyiron_continuum.fenics.job.generic'
 JOB_CLASS_DICT['FenicsLinearElastic'] = 'pyiron_continuum.fenics.job.elastic'
