6a06b510eeb184177152700a531c1e79aedc9115
Liam Huber <liam.huber@gmail.com>
2021-09-23 11:27:22 +0200

    More (but not complete) type hinting

--- pyiron_continuum/schroedinger/schroedinger.py
+++ pyiron_continuum/schroedinger/schroedinger.py
@@ -13,6 +13,8 @@ from scipy.sparse.linalg import eigsh, LinearOperator
 from abc import ABC, abstractmethod
 from scipy.constants import physical_constants
 import matplotlib.pyplot as plt
+from typing import Union, Type
+from pyiron_continuum.schroedinger.potentials import Potential
 
 try:
     import k3d
@@ -65,11 +67,11 @@ class _TISEInput(DataContainer):
         self._mesh = new_mesh
 
     @property
-    def potential(self):
+    def potential(self) -> Union[Type[Potential], np.ndarray]:
         return self._potential
 
     @potential.setter
-    def potential(self, new_potential):
+    def potential(self, new_potential: Union[Type[Potential], np.ndarray]):
         self._potential = new_potential
 
 
@@ -82,27 +84,27 @@ class _TISEOutput(DataContainer):
         self._rho = None
 
     @property
-    def _space_axes(self):
+    def _space_axes(self) -> tuple:
         return tuple(n for n in range(1, len(self.psi.shape)))
 
-    def _broadcast_vector_to_space(self, vector):
+    def _broadcast_vector_to_space(self, vector: np.ndarray) -> np.ndarray:
         return vector.reshape(-1, *(1,)*len(self._space_axes))
 
-    def _weight_states(self, states, weights):
+    def _weight_states(self, states: np.ndarray, weights: np.ndarray) -> np.ndarray:
         return states * self._broadcast_vector_to_space(weights)
 
     @property
-    def rho(self):
+    def rho(self) -> np.ndarray:
         if self._rho is None and self.psi is not None:
             self._rho = self.psi ** 2
         return self._rho
 
-    def get_boltzmann_occupation(self, temperature):
+    def get_boltzmann_occupation(self, temperature: float) -> np.ndarray:
         if self.energy is not None:
             w = np.exp(-(self.energy - self.energy.min()) / (KB * temperature))
             return w / w.sum()
 
-    def get_boltzmann_rho(self, temperature):
+    def get_boltzmann_rho(self, temperature: float) -> np.ndarray:
         if self.psi is not None:
             w = self.get_boltzmann_occupation(temperature)
             return self._weight_states(self.rho, w).sum(axis=0)
@@ -147,11 +149,11 @@ class TISE(PythonTemplateJob):
         self.storage.output = _TISEOutput()
 
     @property
-    def mesh(self):
+    def mesh(self) -> RectMesh:
         return self.input.mesh
 
     @property
-    def potential(self):
+    def potential(self) -> Union[Type[Potential], np.ndarray]:
         return self.input.potential
 
     @property
@@ -166,13 +168,13 @@ class TISE(PythonTemplateJob):
     def plot_3d(self):
         return _Plot3D(self)
 
-    def _potential_psi(self, psi):
+    def _potential_psi(self, psi) -> np.ndarray:
         try:
             return self.potential(self.mesh) * psi
         except TypeError:
             return self.potential * psi
 
-    def _hamiltonian(self, psi):
+    def _hamiltonian(self, psi: np.ndarray) -> np.ndarray:
         """
         H = -(hbar^2 / 2 m)(del^2 / del x^2) + V
 
@@ -187,7 +189,7 @@ class TISE(PythonTemplateJob):
         return -(HBAR ** 2 / (2 * self.input.mass)) * self.mesh.laplacian(psi) * EV2_S2_PER_ANG2_PER_AMU_IN_EV + \
                self._potential_psi(psi)
 
-    def _flat_hamiltonian(self, psi_1d):
+    def _flat_hamiltonian(self, psi_1d: np.ndarray) -> np.ndarray:
         """Matrix-vector product for `LinearOperator` to use."""
         return self._hamiltonian(psi_1d.reshape(self.mesh.shape[1:])).flatten()
 
