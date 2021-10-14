ffa607954e390155a45538241f9f814cb261de92
muh-hassani <mhassani86@gmail.com>
2021-10-15 00:36:54 +0200

    enriching job.output, adding plotting

--- pyiron_continuum/damask/damaskjob.py
+++ pyiron_continuum/damask/damaskjob.py
@@ -11,6 +11,8 @@ with ImportAlarm(
 ) as damask_alarm:
     from damask import Result
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 __author__ = "Muhammad Hassani"
@@ -98,8 +100,26 @@ class DAMASK(TemplateJob):
         if self._results is None:
             if file_name != "damask_loading.hdf5":
                 self._damask_hdf = os.path.join(self.working_directory, file_name)
-            self._results = Result(self._damask_hdf)
-        return self._results
+
+        self._results = Result(self._damask_hdf)
+        self._results.add_stress_Cauchy()
+        self._results.add_strain()
+        self._results.add_equivalent_Mises('sigma')
+        self._results.add_equivalent_Mises('epsilon_V^0.0(F)')
+        self._results.add_calculation(name='avg_sigma', formula="np.average(#sigma#, axis=0)",
+                                      unit='Pa',description='average stress')
+        self._results.add_calculation(name='avg_strain', formula="np.average(#epsilon_V^0.0(F)#, axis=0)",
+                                      unit='', description='average strain')
+        self._results.add_calculation(name='avg_sigma_vM', formula="np.average(#sigma_vM#)",
+                                      unit='Pa',description='average stress VM')
+        self._results.add_calculation(name='avg_strain_vM', formula="np.average(#epsilon_V^0.0(F)_vM#)",
+                                      unit='', description='average strain vM')
+        self.output.stress = np.array([val for val in self._results.get('avg_sigma').values()])
+        self.output.strain = np.array([val for val in self._results.get('avg_strain').values()])
+        self.output.stress_von_Mises = np.array([val for val
+                                                 in self._results.get('avg_sigma_vM').values()])
+        self.output.strain_von_Mises = np.array([val for val
+                                                 in self._results.get('avg_strain_vM').values()])
 
     @staticmethod
     def list_solvers():
@@ -109,3 +129,40 @@ class DAMASK(TemplateJob):
         return [{'mechanical': 'spectral_basic'},
                 {'mechanical': 'spectral_polarization'},
                 {'mechanical': 'FEM'}]
+
+    def plot_stress_strain(self, component=None, von_mises=False):
+        """
+        Plot the stress strain curve from the job file
+        Parameters
+        ----------
+        direction(str): 'xx, xy, xz, yx, yy, yz, zx, zy, zz
+        """
+        fig, ax = plt.subplots()
+        if component is not None:
+            if von_mises is True:
+                raise ValueError("It is not allowed that component is specified and von_mises is also True ")
+            if len(component) != 2:
+                ValueError("The length of direction must be 2, like 'xx', 'xy', ... ")
+            if component[0] != 'x' or component[0] != 'y' or component[0] != 'z':
+                ValueError("The direction should be from x, y, and z")
+            if component[1] != 'x' or component[1] != 'y' or component[1] != 'z':
+                ValueError("The direction should be from x, y, and z")
+            _component_dict={'x': 0, 'y': 1, 'z': 2}
+            _zero_axis = int(_component_dict[component[0]])
+            _first_axis = int(_component_dict[component[1]])
+            ax.plot(self.output.strain[:, _zero_axis, _first_axis],
+                    self.output.stress[:, _zero_axis, _first_axis],
+                    linestyle='-', linewidth='2.5')
+            ax.grid(True)
+            ax.set_xlabel(rf'$\varepsilon_{component[0]}$' + rf'$_{component[1]}$', fontsize=18)
+            ax.set_ylabel(rf'$\sigma_{component[0]}$' + rf'$_{component[1]}$' + '(Pa)', fontsize=18)
+        elif von_mises is True:
+            ax.plot(self.output.strain_von_Mises, self.output.stress_von_Mises,
+                    linestyle='-', linewidth='2.5')
+            ax.grid(True)
+            ax.set_xlabel(r'$\varepsilon_{vM}$', fontsize=18)
+            ax.set_ylabel(r'$\sigma_{vM}$ (Pa)', fontsize=18)
+        else:
+            raise ValueError("either direction should be passed in "
+                             "or vonMises should be set to True")
+        return fig, ax
