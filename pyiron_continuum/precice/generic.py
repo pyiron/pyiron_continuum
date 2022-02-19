0df71c2d5fced0a268ac99e553c386214ab6b157
muh-hassani <mhassani86@gmail.com>
2022-02-19 22:34:19 +0100

    updating precice/generic.py

--- pyiron_continuum/precice/generic.py
+++ pyiron_continuum/precice/generic.py
@@ -1,3 +1,12 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+"""
+A job class for coupling multiple simulation processes via precice (precice.org) .
+"""
+
+
 from pyiron_base import ImportAlarm
 from pyiron_base.master.generic import GenericMaster
 with ImportAlarm(
@@ -5,7 +14,19 @@ with ImportAlarm(
 ) as precice_alarm:
     import precice
     import pyprecice
+
 from multiprocessing import Process
+
+__author__ = "Muhammad Hassani"
+__copyright__ = (
+    "Copyright 2022, Max-Planck-Institut für Eisenforschung GmbH - "
+    "Computational Materials Design (CM) Department"
+)
+__version__ = "0.1"
+__maintainer__ = "Muhammad Hassani"
+__email__ = "hassani@mpie.de"
+__status__ = "development"
+__date__ = "Feb 19, 2022"
 
 
 class Precice(GenericMaster):
@@ -18,6 +39,7 @@ class Precice(GenericMaster):
             self.child_ids.append(job.id)
 
     def run_static(self):
+
         self.sync_child_jobs()
         processes = []
         for job in self.child_list:
@@ -31,5 +53,6 @@ class Precice(GenericMaster):
         for process in processes:
             process.close()
         self.status.finished = True
+
     def write_input(self):
         pass
