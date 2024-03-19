2340330616eeef9e5fadb09200031d33e5acfbe8
samwaseda <o.waseda@mpie.de>
2024-03-19 18:35:59 +0000

    refactor hdf path

--- pyiron_continuum/damask/damaskjob.py
+++ pyiron_continuum/damask/damaskjob.py
@@ -36,7 +36,6 @@ class DAMASK(TemplateJob):
             job_name(str): the name of the job
         """
         super(DAMASK, self).__init__(project, job_name)
-        self._damask_hdf = os.path.join(self.working_directory, "damask_loading_material.hdf5")
         self._material = None
         self._loading = None
         self._grid = None
@@ -97,11 +96,11 @@ class DAMASK(TemplateJob):
             Args:
                 file_name(str): path to the hdf file
         """
-        if self._results is None:
-            if file_name != "damask_loading_material.hdf5":
-                self._damask_hdf = os.path.join(self.working_directory, file_name)
+        if self._results is not None:
+            return 
+        damask_hdf = os.path.join(self.working_directory, file_name)
 
-        self._results = Result(self._damask_hdf)
+        self._results = Result(damask_hdf)
         self._results.add_stress_Cauchy()
         self._results.add_strain()
         self._results.add_equivalent_Mises('sigma')
