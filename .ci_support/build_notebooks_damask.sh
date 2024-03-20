6b2b370f8c798a1bca3fbdf839162fb78803b0be
mbruns91 <marianbruns91@gmail.com>
2024-03-20 13:58:02 +0100

    create and edit build_notebooks_damask.sh, build_notebooks_damask.sh, exclude_damask and exclude_fenics

---
+++ .ci_support/build_notebooks_damask.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+for f in $(cat .ci_support/exclude_damask); do 
+    rm "notebooks/$f";     
+done;
+# execute notebooks
+i=0;
+cd notebooks
+for notebook in $(ls ./*.ipynb); do
+      papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k "python3" || i=$((i+1));
+done;
+
+# push error to next level
+if [ $i -gt 0 ]; then
+    exit 1;
+fi;
