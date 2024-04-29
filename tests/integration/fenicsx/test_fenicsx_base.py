f324792ff1c2ec8ab03cc04c0db7e18458edaf44
samwaseda <o.waseda@mpie.de>
2024-04-29 16:10:40 +0000

    add tests

---
+++ tests/integration/fenicsx/test_fenicsx_base.py
@@ -0,0 +1,23 @@
+import unittest
+from pyiron_continuum import Project
+import os
+
+
+class TestDamask(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.file_location = os.path.dirname(os.path.abspath(__file__))
+        cls.project = Project("FENICSX")
+        cls.project.remove_jobs(recursive=True, silently=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.project.remove(enable=True)
+
+    def test_load(self):
+        job = self.project.create.job.Fenicsx("test")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
