694924051eee7c248248440fffd02638f15a07f6
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-08-02 09:13:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'pyiron_base==0.9.10',
         'pyiron_snippets==0.1.3',
         'scipy==1.14.0',
-        'sympy==1.13.1'
+        'sympy==1.13.0'
     ],
     extras_require={
         'fenics': [
