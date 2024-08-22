0fc288c63780e7cd31281b4bb515f34c7cfb94d0
samwaseda <o.waseda@mpie.de>
2024-08-22 12:46:01 +0000

    copy setup.py from pyiron_module_template

--- setup.py
+++ setup.py
@@ -1,51 +1,8 @@
-"""
-Setuptools based setup module
-"""
-from setuptools import setup, find_packages
+from setuptools import setup
+
 import versioneer
 
-
 setup(
-    name='pyiron_continuum',
     version=versioneer.get_version(),
-    description='Repository for user-generated plugins to the pyiron IDE.',
-    long_description='http://pyiron.org',
-
-    url='https://github.com/pyiron/pyiron_continuum',
-    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
-    author_email='huber@mpie.de',
-    license='BSD',
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Topic :: Scientific/Engineering :: Physics',
-        'License :: OSI Approved :: BSD License',
-        'Intended Audience :: Science/Research',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11'
-    ],
-
-    keywords='pyiron',
-    packages=find_packages(exclude=["*tests*"]),
-    install_requires=[
-        'matplotlib==3.9.1',
-        'numpy==1.26.4',
-        'pyiron_base==0.9.12',
-        'pyiron_snippets==0.1.3',
-        'scipy==1.14.0',
-        'sympy==1.13.1'
-    ],
-    extras_require={
-        'fenics': [
-            'fenics==2019.1.0',
-            'mshr==2019.1.0',
-        ],
-        'schroedinger': ['k3d==2.16.1']
-    },
     cmdclass=versioneer.get_cmdclass(),
-    
 )
