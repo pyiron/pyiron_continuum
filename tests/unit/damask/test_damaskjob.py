be1516026e42a559b13d86e314530f6ff463eb15
samwaseda <o.waseda@mpie.de>
2024-09-03 09:24:41 +0000

    remove tests

--- tests/unit/damask/test_damaskjob.py
+++ tests/unit/damask/test_damaskjob.py
@@ -61,12 +61,6 @@ class TestDamask(PyironTestCase):
         )
         self.assertIsInstance(job.input.phase, dict)
 
-    def test_composition_to_spacegroup(self):
-        self.assertEqual(composition_to_spacegroup("Aluminum"), "FCC")
-        self.assertEqual(composition_to_spacegroup("Iron"), "BCC")
-        self.assertEqual(composition_to_spacegroup("Magnesium"), "HEX")
-        self.assertRaises(NameError, composition_to_spacegroup, "Frodo")
-
     @classmethod
     def tearDownClass(cls):
         cls.project.remove(enable=True)
