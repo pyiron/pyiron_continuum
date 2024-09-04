76f5e6d5de75141028fdec5873eaaf2ff07a4037
samwaseda <o.waseda@mpie.de>
2024-09-04 07:09:04 +0000

    add tests

---
+++ tests/unit/damask/test_reference.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Copyright (c) Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department
+# Distributed under the terms of "New BSD License", see the LICENSE file.
+
+import unittest
+from pyiron_continuum.damask.reference.yaml import get_plasticity, get_elasticity
+import unittest
+
+
+class TestReference(unittest.TestCase):
+    def test_content(self):
+        plasticity = get_plasticity()
+        self.assertIsInstance(plasticity, dict)
+        self.assertGreater(len(plasticity), 0)
+
+        elasticity = get_elasticity()
+        self.assertIsInstance(elasticity, dict)
+        self.assertGreater(len(elasticity), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
